1ca86a1a6b4188dd5b8a396cd69ff44d7d5dbaac
Iman0har <manoharrao567@gmail.com>
2022-02-07 21:37:55 +0530

    aa
    mm

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import {Form} from "./components/Form"
+
+function App() {
+  return (
+    <div className="App">
+       hello world
+      <Form  data="formData" />
+     
+     
+   
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Form.jsx
@@ -0,0 +1,109 @@
+import { useEffect, useState } from "react"
+
+ export const Form = ()=>{
+const [formData,setFormData] = useState({
+    gamename :"",
+    gameauthor:"",
+    gameprice:"",
+    gametags:[],
+    forkids:"",
+    gamedesc:"",
+    gamerating:""
+}) 
+
+const [totaldata,setTotal] =useState([])
+
+useEffect(()=>{
+    getdata()
+},[])
+
+ const getdata=()=>{
+    fetch("http://localhost:3001/games").then((d)=>d.json()).then((res)=>{setTotal(res)})
+}
+
+  const handlechange =(e)=>{
+    var {name} = e.target
+
+    setFormData({
+        ...formData,
+        [name]:e.target.value
+    })
+  }
+
+    return (
+
+        <div>
+        <form id="addgame"  onSubmit={
+            (e)=>{
+            // e.preventDefault();
+            console.log(formData)
+
+            fetch("http://localhost:3001/games",{
+                method:"POST",
+                body:JSON.stringify(formData),
+                headers:{
+                    "content-type":"application/json"
+                }
+
+            })
+        
+            }
+
+        }>
+         <input name="gamename" onChange={handlechange} type="text" placeholder="gamename"/>
+         <input name="gameauthor" onChange={handlechange} type="text" placeholder="gameauthor "/>
+         <input name="gameprice" onChange={handlechange} type="number" placeholder="gameprice"/>
+         <input name="gametags" onChange={handlechange} type="text" placeholder="gametags"/>
+         <input  name="forkids" onChange={handlechange} type="checkbox" placeholder="forkids"
+            />
+           <textarea  name="gamedesc" onChange={handlechange} placeholder="gamedesc"></textarea> 
+         <select name="gamerating" id="">
+             <option value="1">1</option>
+             <option value="2">2</option>
+             <option value="3">3</option>
+             <option value="4">4</option>
+             <option value="5">5</option>
+         </select>
+         
+         <input type="submit" onChange={handlechange} name="" id="" placeholder="submit" />
+
+        
+        
+     
+        </form>
+            
+        <table id="table"  
+     >
+      
+         <thead  id="tr">
+             <tr>
+               <th>gamename </th>
+               <th>gameauthor</th>
+               <th>gameprice</th>
+               <th>gametags</th>
+               <th>forkids</th>
+               <th>gamedesc</th>
+               <th>gamerating</th>
+             
+             </tr>
+            
+         </thead>
+         <tbody  >
+         
+         
+         
+         {totaldata.map((e)=>(
+            <tr className={"gamerow"}>
+              {Object.keys(e).map((key)=>{if(key == "gameprice"){
+                return <td className={"gameprice"}>{e[key]}</td>
+              }else if(key === "gamerating"){
+                return <td className={"gamerating"}>{e[key]}</td>
+              }else{return<td>{e[key]}</td>}})}
+            </tr>
+         ))}
+
+</tbody>
+        </table>
+        </div>
+    )
+}
